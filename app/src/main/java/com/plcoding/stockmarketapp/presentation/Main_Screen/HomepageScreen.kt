bc4950b7527a659e37f54d14c109341d39040892
charlesson7777 <123232898+charlesson7777@users.noreply.github.com>
2024-11-18 00:05:50 -0500

    first commit

---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/Main_Screen/HomepageScreen.kt
@@ -0,0 +1,19 @@
+package com.plcoding.stockmarketapp.presentation.Main_Screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.plcoding.stockmarketapp.domain.model.CompanyListing
+
+@Composable
+
+
+
