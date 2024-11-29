50d5957fd9e6ac84c8f84a0398fa87fb971e9504
jiahaohe <jiahhe@iu.edu>
2024-11-28 22:01:04 -0500

    DEBUG: Parse Filter

--- app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -13,7 +13,7 @@ interface StockApi {
         @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
 
-    @GET("query?function=TIME_SERIES_INTRADAY&interval=15min&datatype=csv")
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
     suspend fun getIntradayInfo(
         @Query("symbol") symbol: String,
         @Query("apikey") apiKey: String = API_KEY
